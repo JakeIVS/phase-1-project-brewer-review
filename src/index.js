213533530e0007ec5f0b8c7e51c7a98e137b28f1
Jacob Ives <jacob.w.ives@gmail.com>
2023-05-15 14:31:23 -0600

    clean up javascript

--- src/index.js
+++ src/index.js
@@ -1,51 +1,16 @@
-// // const breweryList = document.querySelector('#brewery-list');
-// document.addEventListener("DOMContentLoaded", initialize)
-
-// function initialize() {
-//     // console.log("initalize")
-//     fetch('https://api.openbrewerydb.org/breweries')
-//     .then(r=>r.json())
-//     .then(data=>{
-//         data.forEach(brewery => {
-//             listElement(brewery.name, brewery.state)
-//         })
-//     })
-// }
-
-// function testApi() {
-//     fetch('https://api.openbrewerydb.org/breweries')
-//     .then(r=>r.json())
-//     .then(data=>data.forEach(d=>{
-//         // console.log(d.state);
-//     }))
-// }
-
-// testApi();
-
-function listElement(breweryName,state){
-    // console.log ("creatingElement")
-    let li = document.createElement('li');
-    li.textContent=`${breweryName}, ${state}`;
-    document.querySelector("#brewery-list").appendChild(li);
-};
-
+document.addEventListener("DOMContentLoaded", initialize())
 const breweryList = document.querySelector('#brewery-list');
-
-function initialize() {
-    fetch('https://api.openbrewerydb.org/breweries')
-const breweryList = document.querySelector('#brewery-list');
-document.addEventListener("DOMContentLoaded", ()=>{
-    initialize()
-})
-
 function initialize() {
     fetch('https://api.openbrewerydb.org/v1/breweries?by_state=colorado&per_page=700')
     .then(r=>r.json())
     .then(data=>{
         data.forEach(brewery => {
-            let li = document.createElement('li');
-            li.textContent=`${brewery.name}, ${brewery.city}`;
-            breweryList.append(li);
+            listElement(brewery.name, brewery.city)
         })
     })
-}}
+}
+function listElement(breweryName,city){
+    let li = document.createElement('li');
+    li.textContent=`${breweryName}, ${city}`;
+    document.querySelector("#brewery-list").appendChild(li);
+};
