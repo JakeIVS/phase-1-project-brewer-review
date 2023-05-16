3c9246b8af3ed5dbccffc1cbe1e9c7c3dd90f1a1
tgartland89 <thomas.gartland@gmail.com>
2023-05-15 23:31:05 -0600

    updated filter form

--- src/index.js
+++ src/index.js
@@ -1,78 +1,48 @@
 document.addEventListener('DOMContentLoaded', initialize);
-const breweryList = document.querySelector('#brewery-list');
-let originalData = []; // Store the original data for filtering
+let breweryList = document.querySelector('#brewery-list'); 
+let originalData = []; // this stores the original data from the API for filtering, I was missing earlier causing issues
 
 function initialize() {
   fetch('https://api.openbrewerydb.org/v1/breweries?by_state=colorado&per_page=700')
     .then(r => r.json())
     .then(data => {
-      originalData = data; // Store the original data for filtering
+      originalData = data; // this stores the original data for filtering
       data.forEach(brewery => {
         listElement(brewery.name, brewery.city);
+        // had a cities.push here, but removed it- again causing hicups 
       });
     });
 }
+
 function listElement(breweryName, city) {
     let li = document.createElement('li');
     li.textContent = `${breweryName}, ${city}`;
     document.querySelector("#brewery-list").appendChild(li);
   }
-  
-  function brewFilter(cityValue, typeValue) {
-    const filteredList = originalData.filter(function (brewery) {
-      const isCityMatched = cityValue === '' || brewery.city.toLowerCase().includes(cityValue.toLowerCase());
-      const isTypeMatched = typeValue === 'null' || brewery.brewery_type === typeValue;
+
+  //  added cityValues and typeValue to function- was only including city before also took out additional fetch
+// it was filtering dtata but not updating duplayed list
+  function brewFilter(cityValue, typeValue) { 
+    let filteredList = originalData.filter(function (brewery) {
+      let isCityMatched = cityValue === '' || brewery.city.toLowerCase().includes(cityValue.toLowerCase());
+      let isTypeMatched = typeValue === 'null' || brewery.brewery_type === typeValue;
       return isCityMatched && isTypeMatched;
     })
-    // Clear existing list
+
+// Clears existing list
   breweryList.innerHTML = '';
 
-  // Display filtered results
+// Displays filtered results
   filteredList.forEach(brewery => {
     listElement(brewery.name, brewery.city);
   });
 }
-
-const form = document.querySelector("#filter-form");
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const cityValue = form.querySelector("#city-field").value;
-  const typeValue = form.querySelector("select").value;
-  brewFilter(cityValue, typeValue);
+// sets p filter form  to search city and type and added a retrieve to pass it through beerFilter 
+    let form = document.querySelector("#filter-form");
+        form.addEventListener('submit', (e) => {
+        e.preventDefault();
+    let cityValue = form.querySelector("#city-field").value;
+    let typeValue = form.querySelector("select").value;
+    
+    brewFilter(cityValue, typeValue);
 });
-
-// document.addEventListener('DOMContentLoaded', initialize())
-// const breweryList = document.querySelector('#brewery-list');
-// function initialize() {
-//     fetch('https://api.openbrewerydb.org/v1/breweries?by_state=colorado&per_page=700')
-//     .then(r=>r.json())
-//     .then(data=>{
-//         const cities = [];
-//         data.forEach(brewery => {
-//             listElement(brewery.name, brewery.city)
-//             cities.push(brewery.city)
-//         }) 
-//         // console.log (cities);
-//     })
-// }
-// function listElement(breweryName,city){
-//     let li = document.createElement('li');
-//     li.textContent=`${breweryName}, ${city}`;
-//     document.querySelector("#brewery-list").appendChild(li);
-// };
-// function brewFilter(cityValue){
-//     fetch('https://api.openbrewerydb.org/v1/breweries?by_state=colorado&per_page=700')
-//     .then(r=>r.json())
-//     .then(data=> {
-//         newList = data.filter(function(brewery){
-//             return brewery.city === cityValue 
-//         })
-//         console.log(newList);
-//     })
-// }
-// form = document.querySelector("#filter-form")
-// form.addEventListener('submit',(e) =>{
-//     e.preventDefault();
-//     let cityValue = form.querySelector("#city-field").value 
-//     brewFilter(cityValue);
-// })
